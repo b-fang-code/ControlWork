03a7f9f1ccc26b803d40627961f1bd3c12878eb4
b_fang <shampuk@gmail.com>
2022-12-24 15:23:12 +0300

    write code for task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] sourceArray = new string[] { "Hello", "77", "Peace", "Cat", "=)" };
+string[] resultArray = new string[sourceArray.Length];
+Console.WriteLine("Исходный массив: ");
+PrintArray(sourceArray);
+SortArray(sourceArray,resultArray);
+Console.WriteLine("Результат: ");
+PrintArray(resultArray);
+
+
+//////////////  Method ////////////////////
+void SortArray(string[] firstArray, string[] secondArray)
+{
+    int j = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            secondArray[j] = firstArray[i];
+            j++;
+        }
+    }
+}
+
+
+//////////////  Method ////////////////////
+void PrintArray (string [] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
